68b1b5e2c3c965afd4c1416bfca6664312406561
Jarrett GXZ <jarrettgoh.xz@gmail.com>
2025-01-19 12:31:49 +0800

    Created new folder structure

---
+++ lab assignments/lab2.c
@@ -0,0 +1,20 @@
+/*
+Write a C program that reads in several lines of non-negative integer numbers,
+computes the average for each line and prints out the average. The value –1 in each line
+of user input is used to indicate the end of input for that line.
+A sample program template is given below.
+
+Sample input and output sessions are given below:
+(1) Test Case 1:
+Enter number of lines:
+1
+Enter line 1 (end with -1):
+1 2 3 4 -1
+Average = 2.50
+*/
+
+#include <stdio.h>
+int main()
+{
+    return 0;
+}
