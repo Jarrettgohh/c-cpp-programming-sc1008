b7e7aee784a6ebca2a38e6afb67901298a857f4a
Jarrett GXZ <jarrettgoh.xz@gmail.com>
2025-01-19 15:34:41 +0800

    Added code for lab2

--- lab assignments/lab2.c
+++ lab assignments/lab2.c
@@ -14,7 +14,57 @@ Average = 2.50
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// clear stdin buffer to prevent interferences with subsequent getchar() calls
+void clear_input_buffer()
+{
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        // Keep reading and discarding characters until newline or EOF
+    }
+}
+
 int main()
 {
+
+    for (;;)
+    {
+
+        int numLine;
+
+        printf("Enter number of lines:\n");
+        scanf("%d", &numLine);
+
+        for (int i = 0; i < numLine; i++)
+        {
+
+            printf("Enter Line %d (end with -1):\n", i + 1);
+
+            int input;
+            int total = 0, count = 0;
+            float avg;
+
+            while (scanf("%d", &input) == 1)
+            {
+
+                if (input == -1)
+                    break;
+
+                total += input;
+                count += 1;
+            }
+
+            clear_input_buffer();
+
+            avg = (float)total / count;
+
+            printf("Average = %.2f\n", avg);
+        }
+
+        printf("\n");
+    }
+
     return 0;
 }
