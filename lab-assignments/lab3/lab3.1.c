e0ef00b27778c2e816ff96c6e79075a54f53f31b
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2025-02-16 12:27:33 +0800

    -

--- lab-assignments/lab3/lab3.1.c
+++ lab-assignments/lab3/lab3.1.c
@@ -2,29 +2,29 @@
 
 void reverseAr1D(int ar[], int size);
 
-
 int main()
 {
 
- int ar[10];
- int size, i;
+    int ar[10];
+    int size, i;
 
- printf("Enter array size: \n");
- scanf("%d", &size);
- printf("Enter %d data: \n", size);
- for (i=0; i <= size-1; i++)
- scanf("%d", &ar[i]);
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+    printf("Enter %d data: \n", size);
+    for (i = 0; i <= size - 1; i++)
+        scanf("%d", &ar[i]);
 
- reverseAr1D(ar, size);
- printf("reverseAr1D(): ");
+    reverseAr1D(ar, size);
+    printf("reverseAr1D(): ");
 
- if (size > 0) {
+    if (size > 0)
+    {
 
- for (i=0; i<size; i++)
- printf("%d ", ar[i]);
- }
+        for (i = 0; i < size; i++)
+            printf("%d ", ar[i]);
+    }
 
- return 0;
+    return 0;
 }
 
 // 0, 1, 2, 3, 4, 5 (6) -> 0,5 1,4 2,3 -> 5,3,1
@@ -33,17 +33,15 @@ int main()
 void reverseAr1D(int ar[], int size)
 {
 
-int stopIndex = (size/2) - 1;
+    int stopIndex = (size / 2) - 1;
 
- for (int i = 0; i <= stopIndex; i++) {
-     int start = i;
-     int stop = start + ((size-1)-(i*2));
+    for (int i = 0; i <= stopIndex; i++)
+    {
+        int start = i;
+        int stop = start + ((size - 1) - (i * 2));
 
-     printf("start: %d\n", start);
-     printf("stop: %d\n", stop);
-
-     int store = ar[start];
-     ar[start] = ar[stop];
-     ar[stop] = store;
- }
+        int store = ar[start];
+        ar[start] = ar[stop];
+        ar[stop] = store;
+    }
 }
--- lab-assignments/lab4/lab4q1.c
+++ lab-assignments/lab4/lab4q1.c
@@ -8,87 +8,87 @@ void remove_spaces(char *str);
 
 int main()
 {
- char str[80], str2[80], *p;
+    char str[80], str2[80], *p;
 
- printf("Enter the string: \n");
- fgets(str, 80, stdin);
- if (p = strchr(str, '\n'))
-  *p = '\0';
+    printf("Enter the string: \n");
+    fgets(str, 80, stdin);
+    if (p = strchr(str, '\n'))
+        *p = '\0';
 
- strcpy(str2, str);
- // printf("sweepSpace1(): %s\n", sweepSpace1(str));
- // printf("sweepSpace2(): %s\n", sweepSpace2(str2));
+    strcpy(str2, str);
+    // printf("sweepSpace1(): %s\n", sweepSpace1(str));
+    // printf("sweepSpace2(): %s\n", sweepSpace2(str2));
 
- // remove_spaces(str2);
- // printf("%s\n", str2);
+    // remove_spaces(str2);
+    // printf("%s\n", str2);
 
- return 0;
+    return 0;
 }
 
 char *sweepSpace1(char *str)
 {
 
- char *sweep = malloc(sizeof(char));
- int i = 0; // str
- int x = 0; // sweep
+    char *sweep = malloc(sizeof(char));
+    int i = 0; // str
+    int x = 0; // sweep
 
- // runs as long as null-terminator not encountered (NOT end of string)
- while (str[i] != 0x0)
- {
+    // runs as long as null-terminator not encountered (NOT end of string)
+    while (str[i] != 0x0)
+    {
 
-  while (str[i] == ' ')
-  {
-   i++;
-  }
+        while (str[i] == ' ')
+        {
+            i++;
+        }
 
-  sweep = realloc(sweep, sizeof(sweep) + sizeof(char));
-  sweep[x++] = str[i++];
- }
+        sweep = realloc(sweep, sizeof(sweep) + sizeof(char));
+        sweep[x++] = str[i++];
+    }
 
- // include null-terminator
- sweep[x] = 0x0;
+    // include null-terminator
+    sweep[x] = 0x0;
 
- return sweep;
+    return sweep;
 }
 
 char *sweepSpace2(char *str)
 {
 
- char *sweep = malloc(2 * sizeof(char));
- char *sweepPtr = sweep; // to keep track of original pointer position
+    char *sweep = malloc(2 * sizeof(char));
+    char *sweepPtr = sweep; // to keep track of original pointer position
 
- if (!sweep)
- {
-  printf("Memory allocation failed!\n");
-  return NULL;
- }
+    if (!sweep)
+    {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
 
- // runs as long as null-terminator not encountered (NOT end of string)
- while (*str != 0x0)
- {
+    // runs as long as null-terminator not encountered (NOT end of string)
+    while (*str != 0x0)
+    {
 
-  while (*str == ' ')
-  {
-   str++;
-  }
+        while (*str == ' ')
+        {
+            str++;
+        }
 
-  *sweep++ = *str++;
- }
+        *sweep++ = *str++;
+    }
 
- // include null-terminator
- *sweep = 0x0;
+    // include null-terminator
+    *sweep = 0x0;
 
- return sweepPtr;
+    return sweepPtr;
 }
 
 void remove_spaces(char *s)
 {
- char *d = s;
- do
- {
-  while (*d == ' ')
-  {
-   ++d;
-  }
- } while (*s++ = *d++);
+    char *d = s;
+    do
+    {
+        while (*d == ' ')
+        {
+            ++d;
+        }
+    } while (*s++ = *d++);
 }
