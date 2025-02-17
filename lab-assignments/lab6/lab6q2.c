1df25eee49db15f2f31615e0790d723359f5f1ba
Jarrettgohh <jarrettgoh.xz@gmail.com>
2025-02-17 17:30:31 +0800

    -

--- lab-assignments/lab6/lab6q2.c
+++ lab-assignments/lab6/lab6q2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int rDigitPos1(int num, int digit);
 void rDigitPos2(int num, int digit, int *pos);
@@ -18,19 +19,42 @@ int main()
 
 int rDigitPos1(int num, int digit)
 {
-
     if (num == 0)
-        return -100;
+        // value 0 will propagate all the way back to the first calling function
+        return 0;
 
-    else if (num % 10 == digit)
+    if (num % 10 == digit)
         return 1;
 
-    int sum = 0;
+    int pos = rDigitPos1(num / 10, digit);
 
-    sum += (1 + rDigitPos1(num / 10, digit));
-
-    return sum;
+    if (pos == 0)
+        // propagated 0
+        return 0;
+    else
+        return pos + 1;
 }
+
+// int rDigitPos1(int num, int digit)
+// {
+//     if (num == 0)
+//         return 0;
+
+//     else if (num % 10 == digit)
+//         return 1;
+
+//     int sum = 0;
+
+//     sum += (1 + rDigitPos1(num / 10, digit));
+
+//     char num_str[80];
+//     sprintf(num_str, "%d", sum);
+
+//     if (strlen(num_str) == sum)
+//         return 0;
+//     else
+//         return sum;
+// }
 
 void rDigitPos2(int num, int digit, int *pos)
 {
