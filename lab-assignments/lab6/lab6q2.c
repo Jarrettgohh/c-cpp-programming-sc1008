5ed2dae7ac150e097b2202b636eccb1ccb8bfa4a
Jarrett Goh XZ <Jarrettgoh.xz@gmail.com>
2025-02-17 15:17:05 +0800

    Create lab6q2.c

---
+++ lab-assignments/lab6/lab6q2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int rDigitPos1(int num, int digit);
+void rDigitPos2(int num, int digit, int *pos);
+
+int main()
+{
+ int number, digit, result=0;
+ printf("Enter the number: \n");
+ scanf("%d", &number);
+ printf("Enter the digit: \n");
+ scanf("%d", &digit);
+ printf("rDigitPos1(): %d\n", rDigitPos1(number, digit));
+ rDigitPos2(number, digit, &result);
+ printf("rDigitPos2(): %d\n", result);
+ return 0;
+}
+
+int p = 1;
+
+int rDigitPos1(int num, int digit)
+{
+ if (num==0){
+     return 0; 
+ }
+ else 
+ {
+     if (num%10 == digit) return p;
+     else
+     { 
+        p++;
+        rDigitPos1(num/10, digit);
+    }
+ }
+}
+
+void rDigitPos2(int num, int digit, int *pos)
+{
+  if (num==0){
+     *pos = 0;
+     return; 
+ }
+ else 
+ {
+    (*pos)++;
+
+     if (num%10 == digit) return;
+     else rDigitPos2(num/10, digit, pos);
+    
+ }
+}
